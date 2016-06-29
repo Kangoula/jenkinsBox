d6e28c3b50e85d74a7b9c40e5943ea0254e9f06d
Guillaume Denis <denisgme@gmail.com>
2016-06-29 15:32:09 +0200

    try to make everything work again because everything decided to stop working

--- setup.sh
+++ setup.sh
@@ -1,15 +1,75 @@
 #!/bin/bash
 
-./scripts/setupDockerRegistry.sh
-./scripts/setupJenkins.sh
+function run {
+  dr=registry
+  dj=jenkins
+  drd=registry/data
+  djd=jenkins/data
+  da=auth
 
-echo ""
-echo "--------------------------------"
-echo "- Running Jenkins and Registry -"
-echo "--------------------------------"
-echo ""
+  if [ ! -d $dr ]
+  then
+    mkdir $dr
+    echo "Directory $dr created"
+  fi
+  
+  if [ ! -d $dj ]
+  then
+    mkdir $dj
+     echo "Directory $dj created"
+  fi
+  
+  if [ ! -d $drd ]
+  then
+    mkdir $drd
+    echo "Directory $drd created"
+  fi
+  
+  if [ ! -d $djd ]
+  then
+    mkdir $djd
+    echo "Directory $djd created"
+  fi
+  
+  if [ ! -d $da ]
+  then
+    mkdir $da
+    echo "Directory $da created"
+  fi
 
-docker-compose up -d
+  ./scripts/setupDockerRegistry.sh
+  ./scripts/setupJenkins.sh
 
-echo "done"
-echo ""
+  echo ""
+  echo "--------------------------------"
+  echo "- Running Jenkins and Registry -"
+  echo "--------------------------------"
+  echo ""
+
+  docker-compose up -d
+
+  echo "done"
+  echo ""
+}
+
+function main {
+  dc=certs
+
+  if [ -d "$dc" ]
+  then
+    if [ ! -f $dc/domain.crt ]
+    then
+      >&2 echo "ERROR: file certs/domain.crt is missing. Aborting"
+    elif [ ! -f $dc/domain.key ]
+    then
+      >&2 echo "ERROR: file certs/domain.key is missing. Aborting"
+    else
+      run
+    fi
+  else
+    >&2 echo "ERROR: the certificates directory is absent"
+    echo "Please add a directory named 'certs' with files domain.crt and domain.key inside"
+  fi
+}
+
+main
